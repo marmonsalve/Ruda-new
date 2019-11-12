e159a075194c1f17361d8f711c425e4f4d5008dd
CJ Dunn <cjdunn@Lappy-447.local>
2019-11-12 13:08:55 -0500

    update build script to also build static fonts

--- sources/build.sh
+++ sources/build.sh
@@ -1,11 +1,12 @@
+
 #!/bin/sh
 set -e
 
 
-# echo "Generating Static fonts"
-# mkdir -p ../fonts
-# fontmake -m Ruda.designspace -i -o ttf --output-dir ../fonts/ttf/
-# fontmake -m Ruda.designspace -i -o otf --output-dir ../fonts/otf/
+echo "Generating Static fonts"
+mkdir -p ../fonts
+fontmake -m Ruda.designspace -i -o ttf --output-dir ../fonts/ttf/
+fontmake -m Ruda.designspace -i -o otf --output-dir ../fonts/otf/
 
 
 echo "Generating VFs"
@@ -17,20 +18,20 @@ rm -rf master_ufo/ instance_ufo/ instance_ufos/*
 
 
 
-# echo "Post processing"
-# ttfs=$(ls ../fonts/ttf/*.ttf)
-# for ttf in $ttfs
-# do
-# 	gftools fix-dsig -f $ttf;
-# 	ttfautohint $ttf "$ttf.fix";
-# 	mv "$ttf.fix" $ttf;
-# done
+echo "Post processing"
+ttfs=$(ls ../fonts/ttf/*.ttf)
+for ttf in $ttfs
+do
+	gftools fix-dsig -f $ttf;
+	ttfautohint $ttf "$ttf.fix";
+	mv "$ttf.fix" $ttf;
+done
 
-# for ttf in $ttfs
-# do
-# 	gftools fix-hinting $ttf;
-# 	#mv "$ttf.fix" $ttf;
-# done
+for ttf in $ttfs
+do
+	gftools fix-hinting $ttf;
+	#mv "$ttf.fix" $ttf;
+done
 
 
 
